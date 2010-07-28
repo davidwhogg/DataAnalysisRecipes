c36475de3cc3dfcf2ee0158d9befdfa7e031d2a7
David W Hogg <david.hogg@nyu.edu>
2010-07-28 19:34:12 +0000

    model complexity: improve non-parametric model in various ways

--- crossvalidate.py
+++ crossvalidate.py
@@ -2,12 +2,14 @@
 # Copyright 2009, 2010 Hogg & Lang
 # for now, all rights reserved
 
+# notes
+# -----
+# - If you don't have pdfjoin, install pdfjam.
+
 # to-do items
 # -----------
 # - switch non-parametric model over to linear interpolation
 #   - this requires synchronized changes to the chi2 *and* wls functions
-# - write gs command that builds multi-page pdf files, of the form
-#   - gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=foo.pdf poly-fits*.pdf
 
 from numpy import *
 # this rc block must be before the matplotlib import?
@@ -47,7 +49,7 @@ def ln_gaussian_1d(x, mx, varx):
 	invvar = 1./varx
 	dx = x-mx
 	d2 = (dx * invvar * dx)
-	return -0.5*log(2.*pi*detvar) -0.5 * d2
+	return -0.5 * log(2.*pi*detvar) - 0.5 * d2
 
 def true_a():
 	return true_a_val
@@ -92,132 +94,11 @@ def poly_wls(x, y, sigmay, order):
 	print 'beta', beta
 	return beta
 
-def ln_loo_stiffline(x, y, sigmay, xa, epsilon, iout):
-	I = (arange(len(x)) != iout)
-	a = stiffline_wls(x[I], y[I], sigmay[I], xa, epsilon)
-	yiout = stiffline(array([x[iout]]), xa, a)
-	return (a, ln_gaussian_1d(y[iout], yiout, sigmay[iout]**2))
-
 def ln_loo_poly(x, y, sigmay, order, iout):
 	I = (arange(len(x)) != iout)
 	a = poly_wls(x[I], y[I], sigmay[I], order)
 	yiout = polynomial(x[iout], a)
 	return (a, ln_gaussian_1d(y[iout], yiout, sigmay[iout]**2))
-
-def plot_stiffline(xa, a, **kwargs):
-	if not 'alpha' in kwargs:
-		kwargs['alpha'] = 0.5
-	if not 'lw' in kwargs:
-		kwargs['lw'] = 2.
-	p = plot(xa, a, 'k-', **kwargs)
-	return p
-
-def stiffline(x, xa, a):
-	return a[stiffline_match(x, xa)]
-
-def stiffline_match(x, xa):
-	matcha= zeros_like(x.astype(int))
-	for ii,xx in enumerate(x):
-		matcha[ii] = argmin((xa-xx)**2)
-	return matcha
-
-def stiffline_chi2(x, y, sigmay, xa, a, epsilon):
-	d = 1
-	ymodel = stiffline(x, xa, a)
-	chi2 = sum(((y - ymodel) / sigmay)**2)
-	chi2 += epsilon * sum((a[:-d] - a[d:])**2)
-	return chi2
-
-# stupid-ass iterative WLS algo; returns best-fit "a"
-def stiffline_wls(x, y, sigmay, xa, epsilon):
-	d = 1
-	maxiter = 1000
-	tolerance = 1e-5
-	matcha = stiffline_match(x, xa)
-	# initialize
-	na = len(xa)
-	a = zeros(na) + mean(y)
-	iter = 0
-	dchi2 = 100 * tolerance
-	chi2 = stiffline_chi2(x, y, sigmay, xa, a, epsilon)
-	while (iter < maxiter) and (dchi2 > tolerance):
-		oldchi2 = chi2
-		for j in range(na):
-			numerator = 0.0
-			denominator = 0.0
-			if (j-d) >= 0:
-				numerator += epsilon * a[j-d]
-				denominator += epsilon
-			if (j+d) < na:
-				numerator += epsilon * a[j+d]
-				denominator += epsilon
-			for (ii,jj) in enumerate(matcha):
-				if jj == j:
-					numerator += y[ii] / sigmay[ii]**2
-					denominator += 1.0 / sigmay[ii]**2
-			a[j] = numerator / denominator
-		chi2 = stiffline_chi2(x, y, sigmay, xa, a, epsilon)
-		dchi2 = oldchi2 - chi2
-		iter += 1
-	print 'iteration', iter
-	print '  chi2', chi2
-	print '  dchi2', dchi2
-	return a
-
-def main_black():
-	print 'making black page, you tool'
-	axes([0,0,1,1], axisbg='k')
-	savefig('black' + plot_format)
-
-def main_stiffline():
-	prefix = 'stiffline-'
-	(x, y, sigmay) = get_data_no_outliers()
-	xa = linspace(xlimits[0], xlimits[1], 40)
-	log2epslist = (range(12))[::-1]
-	nepsilon = len(log2epslist)
-	ln_crossval_like = zeros(nepsilon)
-	for ii,log2eps in enumerate(log2epslist):
-		epsilon = 2.0**log2eps
-		print 'working on epsilon = %f' % epsilon
-
-		clf()
-		plot_yerr(x, y, sigmay)
-		plot_stiffline(xa, stiffline_wls(x, y, sigmay, xa, epsilon))
-		xlim(*xlimits)
-		ylim(*ylimits)
-		title('$K = %i$ ; $\log_2(\epsilon) =  %i$' % (len(xa), log2eps));
-		savefig(prefix + 'log2eps-%02i' % ii + plot_format)
-
-		loo = [ln_loo_stiffline(x, y, sigmay, xa, epsilon, iout) for iout in range(len(x))]
-		ln_crossval_like[ii] = sum([lnlike for (a,lnlike) in loo])
-
-		clf()
-		plot_yerr(x, y, sigmay)
-		for a,nil in loo:
-			plot_stiffline(xa, a, **{'alpha':0.25})
-		xlim(*xlimits)
-		ylim(*ylimits)
-		title('$K = %i$ ; $\log_2(\epsilon) =  %i$' % (len(xa), log2eps));
-		savefig(prefix + 'fits-%02i' % ii + plot_format)
-
-	clf()
-	mx = max(ln_crossval_like)
-	I = argsort(-ln_crossval_like)
-	plot(maximum(mx-1000, ln_crossval_like), 'ko-')
-	ylim(mx-20, mx+2)
-	xlim(-1, 15)
-	xlabel('$\log_2(\epsilon)$')
-	ylabel('cross-validation log-likelihood')
-	title('$K = %i$' % len(xa))
-	savefig(prefix + 'crossval' + plot_format)
-
-	cmdstr = gscmd + '%slog2eps.pdf %slog2eps-*.pdf' % (prefix, prefix)
-	print os.system(cmdstr)
-	cmdstr = gscmd + '%sfits.pdf %sfits-*.pdf' % (prefix, prefix)
-	print os.system(cmdstr)
-	cmdstr = gscmd + '%s.pdf poly-data.pdf poly-truth.pdf %slog2eps.pdf %scrossval.pdf' % (prefix, prefix, prefix)
-	print os.system(cmdstr)
-	return
 
 def main_poly():
 	(x, y, sigmay) = get_data_no_outliers()
@@ -307,21 +188,148 @@ def main_poly():
 		title(cvtit)
 		savefig(prefix + 'bic' + plot_format)
 
-		cmdstr = gscmd + '%sorder.pdf %sorder-*.pdf' % (prefix, prefix)
-		print os.system(cmdstr)
-		cmdstr = gscmd + '%sfits.pdf %sfits-*.pdf' % (prefix, prefix)
-		print os.system(cmdstr)
-		cmdstr = gscmd + '%s.pdf %sdata.pdf %schi2.pdf %saic.pdf %sbic.pdf %scrossval.pdf' % (prefix, prefix, prefix, prefix, prefix, prefix)
+		cmdstr = gscmd + '%s.pdf %schi2.pdf %saic.pdf %sbic.pdf %scrossval.pdf' % (prefix, prefix, prefix, prefix, prefix)
 		print os.system(cmdstr)
 	return
 
+def ln_loo_stiffline(x, y, sigmay, xa, epsilon, iout, a=None):
+	I = (arange(len(x)) != iout)
+	a = stiffline_wls(x[I], y[I], sigmay[I], xa, epsilon, a=a)
+	yiout = stiffline(array([x[iout]]), xa, a)
+	return (a, ln_gaussian_1d(y[iout], yiout, sigmay[iout]**2))
+
+def plot_stiffline(xa, a, **kwargs):
+	if not 'alpha' in kwargs:
+		kwargs['alpha'] = 0.5
+	if not 'lw' in kwargs:
+		kwargs['lw'] = 2.
+	p = plot(xa, a, 'k-', **kwargs)
+	return p
+
+def stiffline(x, xa, a):
+	m = stiffline_match(x, xa)
+	mint = floor(m).astype(int)
+	return ((m-mint)*a[mint]
+		+(mint+1-m)*a[mint+1])
+
+def stiffline_match(x, xa):
+	matcha= zeros_like(x.astype(int))
+	for ii,xx in enumerate(x):
+		indx = argmin((xa-xx)**2)
+		if xx < xa[indx]:
+			indx -= 1
+		matcha[ii] = indx+(xx-xa[indx])/(xa[indx+1]-xa[indx])
+	return matcha
+
+def stiffline_chi2(x, y, sigmay, xa, a, epsilon):
+	d = 1
+	ymodel = stiffline(x, xa, a)
+	chi2 = sum(((y - ymodel) / sigmay)**2)
+	chi2 += epsilon * sum((a[:-d] - a[d:])**2)
+	return chi2
+
+# stupid-ass iterative WLS algo; returns best-fit "a"
+def stiffline_wls(x, y, sigmay, xa, epsilon, a=None):
+	d = 1
+	maxiter = 10000
+	tolerance = 1e-6 * epsilon
+	matcha = stiffline_match(x, xa)
+	# initialize
+	na = len(xa)
+	if a == None:
+		a = zeros(na) + mean(y)
+	iter = 0
+	dchi2 = 100 * tolerance
+	chi2 = stiffline_chi2(x, y, sigmay, xa, a, epsilon)
+	while (iter < maxiter) and (dchi2 > tolerance):
+		oldchi2 = chi2
+		for j in range(na):
+			numerator = 0.0
+			denominator = 0.0
+			if (j-d) >= 0:
+				numerator += epsilon * a[j-d]
+				denominator += epsilon
+			if (j+d) < na:
+				numerator += epsilon * a[j+d]
+				denominator += epsilon
+			for (ii,mm) in enumerate(matcha):
+				jj = floor(mm)
+				weight = 0.0
+				if jj == j:
+					weight = (mm - jj) / sigmay[ii]**2
+				if (jj+1) == j:
+					weight = (jj + 1 - mm) / sigmay[ii]**2
+				numerator += weight * y[ii]
+				denominator += weight
+			a[j] = numerator / denominator
+		chi2 = stiffline_chi2(x, y, sigmay, xa, a, epsilon)
+		dchi2 = oldchi2 - chi2
+		iter += 1
+	print 'iteration', iter
+	print '  chi2', chi2
+	print '  dchi2', dchi2
+	return a
+
+def main_stiffline():
+	prefix = 'stiffline-'
+	(x, y, sigmay) = get_data_no_outliers()
+	xa = linspace(xlimits[0], xlimits[1], 80)
+	log2epslist = 12-arange(15)
+	nepsilon = len(log2epslist)
+	ln_crossval_like = zeros(nepsilon)
+	a = None
+	for ii,log2eps in enumerate(log2epslist):
+		epsilon = 2.0**log2eps
+		print 'working on epsilon = %f' % epsilon
+
+		clf()
+		plot_yerr(x, y, sigmay)
+		a = stiffline_wls(x, y, sigmay, xa, epsilon, a=a)
+		plot_stiffline(xa, a)
+		xlim(*xlimits)
+		ylim(*ylimits)
+		title('$K = %i$ ; $\log_2(\epsilon) =  %i$' % (len(xa), log2eps));
+		savefig(prefix + 'log2eps-%02i' % ii + plot_format)
+
+		loo = [ln_loo_stiffline(x, y, sigmay, xa, epsilon, iout, a=a) for iout in range(len(x))]
+		ln_crossval_like[ii] = sum([lnlike for (a,lnlike) in loo])
+
+		clf()
+		plot_yerr(x, y, sigmay)
+		for a,nil in loo:
+			plot_stiffline(xa, a, **{'alpha':0.25})
+		xlim(*xlimits)
+		ylim(*ylimits)
+		title('$K = %i$ ; $\log_2(\epsilon) =  %i$' % (len(xa), log2eps));
+		savefig(prefix + 'fits-%02i' % ii + plot_format)
+
+	clf()
+	mx = max(ln_crossval_like)
+	I = argsort(-ln_crossval_like)
+	plot(log2epslist, ln_crossval_like, 'ko-')
+	ylim(mx-20, mx+2)
+	xlim(min(log2epslist), max(log2epslist))
+	xlabel('$\log_2(\epsilon)$')
+	ylabel('cross-validation log-likelihood')
+	title('$K = %i$' % len(xa))
+	savefig(prefix + 'crossval' + plot_format)
+
+	cmdstr = gscmd + '%slog2eps.pdf %slog2eps-*.pdf' % (prefix, prefix)
+	print os.system(cmdstr)
+	cmdstr = gscmd + '%s.pdf poly-data.pdf %slog2eps.pdf %scrossval.pdf' % (prefix, prefix, prefix)
+	print os.system(cmdstr)
+	return
+
+def main_black():
+	print 'making black page, you tool'
+	axes([0,0,1,1], axisbg='k')
+	savefig('black' + plot_format)
+
 if __name__ == '__main__':
 	args = sys.argv[1:]
-	if 'black' in args:
-		main_black()
-		sys.exit(0)
-	main_black()
-	main_poly()
-	main_stiffline()
-	cmdstr = gscmd + 'crossvalidate.pdf black.pdf poly-order.pdf poly-.pdf poly-truth.pdf poly-fits-*.pdf poly-crossval.pdf black.pdf poly-data.pdf poly-wrong-.pdf black.pdf stiffline-.pdf black.pdf'
+	if not 'pdfjoin' in args:
+#		main_black()
+#		main_poly()
+		main_stiffline()
+	cmdstr = gscmd + 'crossvalidate.pdf black.pdf poly-data.pdf poly-order-*.pdf poly-.pdf poly-truth.pdf poly-fits-*.pdf poly-crossval.pdf black.pdf poly-data.pdf poly-wrong-data.pdf poly-wrong-.pdf black.pdf stiffline-.pdf black.pdf'
 	print os.system(cmdstr)
