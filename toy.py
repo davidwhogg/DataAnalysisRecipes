359cd054bc1be317c9ff77962909c07a570f3953
David W Hogg <david.hogg@nyu.edu>
2011-07-05 00:58:34 +0000

    hierarchical demo: ready to go

--- toy.py
+++ toy.py
@@ -15,46 +15,58 @@ import pylab as plt
 import scipy.optimize as op
 
 def make_truth():
-    nstar = 70
-    ngalaxy = 300
-    ystar = np.random.uniform(-0.5, 3.5, size=nstar)
+    nstar = 100
+    ngalaxy = 1000
+    ystar = np.random.normal(1.2, 1.0, size=nstar)
     xstar = 0.1 * ystar**2 + 0.02 * np.random.normal(size=nstar)
-    classstar = np.zeros(nstar).astype(int)
-    xgalaxy = np.random.uniform(-0.5, 3.5, size=ngalaxy)
+    cstar = np.zeros(nstar).astype(int)
+    xgalaxy = np.random.normal(1.2, 1.0, size=ngalaxy)
     ygalaxy = 0.1 * (xgalaxy - 0.5)**2 + 0.02 * np.random.normal(size=ngalaxy)
-    classgalaxy = np.ones(ngalaxy).astype(int)
+    cgalaxy = np.ones(ngalaxy).astype(int)
     x = np.append(xstar, xgalaxy)
     y = np.append(ystar, ygalaxy)
-    c = np.append(classstar, classgalaxy)
+    c = np.append(cstar, cgalaxy)
     return ((x, y), c)
 
+def noisify(xy, c):
+    nobj = len(c)
+    x,y = xy
+    invvar = 1.0 / (0.4 * np.random.uniform(size=nobj))**2
+    nx = x + np.random.normal(size=nobj)/np.sqrt(invvar)
+    ny = y + np.random.normal(size=nobj)/np.sqrt(invvar)
+    bad = (np.random.uniform(size=nobj) < 0.01)
+    nc = np.zeros(nobj)
+    nc[:] = c[:]
+    nc[bad] = 1 - nc[bad]
+    return ((nx, ny), nc, invvar)
+
 def make_models():
-    x = np.array([])
-    y = np.array([])
-    c = np.array([])
-    tx, ty = np.meshgrid(np.arange(0.0, 0.51, 0.2), np.arange(-0.5, 2.01, 0.2))
-    tx = tx.ravel()
-    ty = ty.ravel()
-    tc = np.zeros(len(tx)).astype(int)
-    x = np.append(x, tx)
-    y = np.append(y, ty)
-    c = np.append(c, tc)
-    tx, ty = np.meshgrid(np.arange(-0.5, 3.01, 0.3), np.arange(0., 0.61, 0.3))
-    tx = tx.ravel()
-    ty = ty.ravel()
-    tc = np.ones(len(tx)).astype(int)
-    x = np.append(x, tx)
-    y = np.append(y, ty)
-    c = np.append(c, tc)
-    return ((x, y), c)
+    ystar = np.arange(-1., 2., 0.05)
+    xstar = 0.15 * ystar**2 + 0.2 * np.random.normal(size=len(ystar))
+    ystar2 = np.arange(-1., 4., 0.2)
+    xstar2 = 0.1 * ystar2**2 + 0.02 * np.random.normal(size=len(ystar2))
+    ystar = np.append(ystar, ystar2)
+    xstar = np.append(xstar, xstar2)
+    cstar = np.zeros(len(ystar)).astype(int)
+    xgalaxy = np.arange(-1., 2., 0.05)
+    ygalaxy = 0.15 * (xgalaxy - 0.45)**2 + 0.2 * np.random.normal(size=len(xgalaxy))
+    xgalaxy2 = np.arange(-1., 4., 0.2)
+    ygalaxy2 = 0.1 * (xgalaxy2 - 0.5)**2 + 0.02 * np.random.normal(size=len(xgalaxy2))
+    xgalaxy = np.append(xgalaxy, xgalaxy2)
+    ygalaxy = np.append(ygalaxy, ygalaxy2)
+    cgalaxy = np.ones(len(ygalaxy)).astype(int)
+    xmod = np.append(xstar, xgalaxy)
+    ymod = np.append(ystar, ygalaxy)
+    cmod = np.append(cstar, cgalaxy)
+    return ((xmod, ymod), cmod)
 
 # serious duck typing in this function
 def ln_likelihood(x, y, invvar, modelxy):
     mx, my = modelxy
     return -0.5 * invvar * ((x - mx)**2 + (y - my)**2)
 
-def map_model(x, y, invvar, modelxy, hyperpars):
-    return np.argmax(ln_posterior(x, y, invvar, modelxy, hyperpars))
+def ml_model(x, y, invvar, modelxy, hyperpars):
+    return np.argmax(ln_likelihood(x, y, invvar, modelxy))
 
 def logsum(x):
     offset = 700 - np.max(x) - np.log(len(x))
@@ -72,64 +84,61 @@ def marginalized_ln_likelihood(x, y, invvar, modelxy, hyperpars, modelclass, c):
 
 def total_marginalized_ln_likelihood(xy, invvar, modelxy, hyperpars, modelclass, c):
     x, y = xy
-    nx = len(x)
-    mll = np.zeros(nx).astype('float')
-    for i in range(nx):
-        mll[i] = marginalized_ln_likelihood(x[i], y[i], invvar[i], modelxy, hyperpars, modelclass, c)
-    return logsum(mll)
+    mll = [marginalized_ln_likelihood(x[i], y[i], invvar[i], modelxy, hyperpars, modelclass, c) for i in range(len(x))]
+    return np.sum(mll)
 
 def objective(hyperpars, xy, invvar, modelxy, modelclass, c):
     return -1. * total_marginalized_ln_likelihood(xy, invvar, modelxy, hyperpars, modelclass, c)
-
-def noisify(xy, c):
-    nobj = len(c)
-    x,y = xy
-    invvar = 1.0 / (0.05 + 0.35 * np.random.uniform(size=nobj))**2
-    nx = x + np.random.normal(size=nobj)/np.sqrt(invvar)
-    ny = y + np.random.normal(size=nobj)/np.sqrt(invvar)
-    bad = (np.random.uniform(size=nobj) < 0.01)
-    nc = np.zeros(nobj)
-    nc[:] = c[:]
-    nc[bad] = 1 - nc[bad]
-    return ((nx, ny), nc, invvar)
 
 def plot_internal(x, y, c, star, galaxy, alpha):
     for i in [1, 0]:
         if i == 0:
             marker = star
+            a = 1.
         else:
             marker = galaxy
+            a = alpha
         I = (c == i)
         if np.sum(I) > 0:
-            plt.plot(x[I], y[I], marker, alpha=alpha)
+            plt.plot(x[I], y[I], marker, alpha=a)
     return None
 
-def plot_class(xy, c, fn, modelxy=None, modelc=None, hpars=None):
+def plot_class(xy, c, fn, title='', modelxy=None, modelc=None, hpars=None):
     x, y = xy
     plt.clf()
-    plot_internal(x, y, c, 'ko', 'bo', 0.25)
+    plot_internal(x, y, c, 'go', 'bo', 0.25)
     if modelxy is not None:
         mx, my = modelxy
-        alphas = np.maximum(0., (hpars + 60. - np.max(hpars)) / 60.)
+        alphasum = [logsum(hpars[modelc == i]) for i in range(2)]
         for i in range(len(mx)):
             if modelc[i] == 0:
                 marker = 'r+'
+                mew = 15. * np.exp(0.5 * (hpars[i] - alphasum[0]))
+                ms = 45. * np.exp(0.5 * (hpars[i] - alphasum[0]))
             else:
+                alpha = np.exp(hpars[i] - alphasum[1])
                 marker = 'rx'
-            plt.plot([mx[i], ], [my[i], ], marker, alpha=alphas[i])
+                mew = 15. * np.exp(0.5 * (hpars[i] - alphasum[0]))
+                ms = 35. * np.exp(0.5 * (hpars[i] - alphasum[0]))
+            plt.plot([mx[i], ], [my[i], ], marker, alpha=1.0, mew=mew, ms=ms)
     plt.xlim(-2., 5.)
     plt.ylim(-1.5, 4.5)
+    plt.title(title)
     plt.savefig(fn)
     print 'plot_class: wrote ' + fn
     return None
 
-def plot_two_classes(xy, truec, plotc, prefix):
+def plot_two_classes(xy, truec, plotc, prefix, method):
     stars = (plotc == 0)
     starxy = (qq[stars] for qq in xy)
-    plot_class(starxy, truec[stars], prefix + '-stars.png')
+    contam = 100. * sum(truec[stars]) / sum(1-truec[stars])
+    title = '%s / stars / contamination %.2f percent' % (method, contam)
+    plot_class(starxy, truec[stars], prefix + '-stars.png', title=title)
     gals = (plotc == 1)
     galxy = (qq[gals] for qq in xy)
-    plot_class(galxy, truec[gals], prefix + '-gals.png')
+    contam = 100. * sum(1-truec[gals]) / sum(truec[gals])
+    title = '%s / galaxies / contamination %.2f percent' % (method, contam)
+    plot_class(galxy, truec[gals], prefix + '-gals.png', title=title)
     return None
 
 def main():
@@ -142,48 +151,45 @@ def main():
     hyperpars = np.ones(len(modelclass)).astype('float')
     plot_class(truexy, trueclass, 'toy-models-truth.png', modelxy=modelxy, modelc=modelclass, hpars=hyperpars)
     plot_class(noisyxy, trueclass, 'toy-models-noisy.png', modelxy=modelxy, modelc=modelclass, hpars=hyperpars)
-    plot_two_classes(noisyxy, trueclass, trueclass, 'toy-true')
+    plot_two_classes(noisyxy, trueclass, trueclass, 'toy-true', 'truth')
     x, y = noisyxy
     maxlclass = np.zeros(len(x)) - 1
     for i in range(len(x)):
-        maxlclass[i] = modelclass[map_model(x[i], y[i], noisyinvvar[i], modelxy, hyperpars)]
-    plot_two_classes(noisyxy, trueclass, maxlclass, 'toy-maxl')
+        maxlclass[i] = modelclass[ml_model(x[i], y[i], noisyinvvar[i], modelxy, hyperpars)]
+    plot_two_classes(noisyxy, trueclass, maxlclass, 'toy-maxl', 'maximum likelihood')
     marginalizedclass = np.zeros(len(x))
     for i in range(len(x)):
         if marginalized_ln_likelihood(x[i], y[i], noisyinvvar[i], modelxy, hyperpars, modelclass, 1) > marginalized_ln_likelihood(x[i], y[i], noisyinvvar[i], modelxy, hyperpars, modelclass, 0):
             marginalizedclass[i] = 1
-    plot_two_classes(noisyxy, trueclass, marginalizedclass, 'toy-marginal')
+    plot_two_classes(noisyxy, trueclass, marginalizedclass, 'toy-marginal', 'flat priors')
 
     # split into star and galaxy models for optimization; optimize
-    for c in [0, 1]:
-        I = (modelclass == c)
-        thisx, thisy = modelxy
-        thismodelxy = (thisx[I], thisy[I])
-        thismodelclass = modelclass[I]
-        thishyperpars = hyperpars[I]
-        J = (trueclass == c)
-        thatx, thaty = noisyxy
-        thisnoisyxy = (thatx[J], thaty[J])
-        thisinvvar = noisyinvvar[J]
-        args = (thisnoisyxy, thisinvvar, thismodelxy, thismodelclass, c)
-        besthyperpars = op.fmin(objective, thishyperpars, args=args, maxiter=300)
-        thishyperpars = besthyperpars - logsum(besthyperpars)
-        print thishyperpars
-        besthyperpars = op.fmin(objective, thishyperpars, args=args, maxiter=300)
-        thishyperpars = besthyperpars - logsum(besthyperpars)
-        print thishyperpars
-        besthyperpars = op.fmin(objective, thishyperpars, args=args, maxiter=300)
-        thishyperpars = besthyperpars - logsum(besthyperpars)
-        print thishyperpars
-        hyperpars[I] = thishyperpars - logsum(thishyperpars) + np.log(len(thishyperpars)) - np.log(len(hyperpars))
-    print hyperpars
-
-    plot_class(truexy, trueclass, 'toy-models-hier-truth.png', modelxy=modelxy, modelc=modelclass, hpars=hyperpars)
-    plot_class(noisyxy, trueclass, 'toy-models-hier-noisy.png', modelxy=modelxy, modelc=modelclass, hpars=hyperpars)
-    for i in range(len(x)):
-        if marginalized_ln_likelihood(x[i], y[i], noisyinvvar[i], modelxy, hyperpars, modelclass, 1) > marginalized_ln_likelihood(x[i], y[i], noisyinvvar[i], modelxy, hyperpars, modelclass, 0):
-            marginalizedclass[i] = 1
-    plot_two_classes(noisyxy, trueclass, marginalizedclass, 'toy-marginal-hier')
+    for maxfit in [30, 40, 50, 60, 80, 100, 200]:
+        for c in [0, 1]:
+            I = np.flatnonzero(modelclass == c)
+            thisx, thisy = modelxy
+            thismodelxy = (thisx[I], thisy[I])
+            thismodelclass = modelclass[I]
+            assert(np.sum(thismodelclass == (1-c)) == 0)
+            thishyperpars = hyperpars[I]
+            J = np.random.permutation(np.flatnonzero(trueclass == c))
+            if len(J) > maxfit:
+                J = J[:maxfit]
+            thatx, thaty = noisyxy
+            thisnoisyxy = (thatx[J], thaty[J])
+            thisinvvar = noisyinvvar[J]
+            args = (thisnoisyxy, thisinvvar, thismodelxy, thismodelclass, c)
+            besthyperpars = op.fmin(objective, thishyperpars, args=args, maxiter=10000)
+            thishyperpars = besthyperpars - logsum(besthyperpars)
+            print thishyperpars
+            hyperpars[I] = thishyperpars - logsum(thishyperpars) + np.log(len(I))
+        print hyperpars
+        plot_class(truexy, trueclass, 'toy-models-hier-truth.png', modelxy=modelxy, modelc=modelclass, hpars=hyperpars)
+        plot_class(noisyxy, trueclass, 'toy-models-hier-noisy.png', modelxy=modelxy, modelc=modelclass, hpars=hyperpars)
+        for i in range(len(x)):
+            if marginalized_ln_likelihood(x[i], y[i], noisyinvvar[i], modelxy, hyperpars, modelclass, 1) > marginalized_ln_likelihood(x[i], y[i], noisyinvvar[i], modelxy, hyperpars, modelclass, 0):
+                marginalizedclass[i] = 1
+        plot_two_classes(noisyxy, trueclass, marginalizedclass, 'toy-marginal-hier', 'hierarchical priors')
 
     return None
 
